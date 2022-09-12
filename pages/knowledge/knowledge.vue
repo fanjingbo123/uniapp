8f05563bbd2649fb363ba7360df66c5ebbe06798
fanjingbo <superboubou123@gamil.com>
2022-09-12 16:50:47 +0800

    change

--- pages/knowledge/knowledge.vue
+++ pages/knowledge/knowledge.vue
@@ -12,42 +12,90 @@
 			</scroll-view>
 			<!-- 右侧滑动区 -->
 			<scroll-view scroll-y="true" :style="{height: wh + 'px'}">
-				<block v-for="(item, j) in medicineList" :key="j">
-					<view class="medicine_msg">
-						<view class="medicine_img">
-							<image
-								src="https://mmbiz.qpic.cn/mmbiz_png/J9oTXkdKsW80C2VQs0icYG0rK9nd8kqslA3mZIvystzqcic8L4LRtyb4bkNJLWFZnqaR4VlGibjHFyR38k9nmjqaA/0?wx_fmt=png">
-							</image>
+				<view v-for="(item, j) in medicineList" :key="j" @click="gotoDetail(item)">
+					<view class="medicine-msg">
+						<!-- 左侧 -->
+						<view class="medicine-msg-left">
+							<image :src="item.medicine_url" class="medicine-pic"></image>
 						</view>
-						<view>
-							<view>{{item.medicine_msg}}</view>
-							<view>xxxxxxxxxxxxxxxxxxxxxxxxxxxxx</view>
+						<!-- 右侧 -->
+						<view class="medicine-msg-right">
+							<!-- 药品名称 -->
+							<view class="medicine-name">药品名称：{{item.medicine_name}}</view>
+							<!-- 药品简介 -->
+							<view class="medicine-info-box">
+								<view class="medicine-info">药品简介：{{item.medicine_msg}}</view>
+							</view>
 						</view>
 					</view>
-				</block>
-
-				<view>yyy</view>
-				<view>yyy</view>
-				<view>yyy</view>
+				</view>
 			</scroll-view>
 		</view>
 	</view>
 </template>
 
+
 <script>
 	export default {
 		data() {
 			return {
+				page: 1,
+				pageSize: 10,
+				total: 0,
 				wh: 0,
 				medicineList: [],
 				cateList: [{
 						cate: '降压药',
 						List: [{
-							medcine_url: '../static/yaopian.png',
+							medicine_url: 'https://mmbiz.qpic.cn/mmbiz_png/J9oTXkdKsW80C2VQs0icYG0rK9nd8kqslA3mZIvystzqcic8L4LRtyb4bkNJLWFZnqaR4VlGibjHFyR38k9nmjqaA/0?wx_fmt=png',
 							medicine_name: '',
 							medicine_msg: '降压药'
-						}, {
-							medcine_url: '../static/yaopian.png',
+						},{
+							medicine_url: 'https://mmbiz.qpic.cn/mmbiz_png/J9oTXkdKsW80C2VQs0icYG0rK9nd8kqslA3mZIvystzqcic8L4LRtyb4bkNJLWFZnqaR4VlGibjHFyR38k9nmjqaA/0?wx_fmt=png',
+							medicine_name: '',
+							medicine_msg: '降压药'
+						},{
+							medicine_url: 'https://mmbiz.qpic.cn/mmbiz_png/J9oTXkdKsW80C2VQs0icYG0rK9nd8kqslA3mZIvystzqcic8L4LRtyb4bkNJLWFZnqaR4VlGibjHFyR38k9nmjqaA/0?wx_fmt=png',
+							medicine_name: '',
+							medicine_msg: '降压药'
+						},{
+							medicine_url: 'https://mmbiz.qpic.cn/mmbiz_png/J9oTXkdKsW80C2VQs0icYG0rK9nd8kqslA3mZIvystzqcic8L4LRtyb4bkNJLWFZnqaR4VlGibjHFyR38k9nmjqaA/0?wx_fmt=png',
+							medicine_name: '',
+							medicine_msg: '降压药'
+						},{
+							medicine_url: 'https://mmbiz.qpic.cn/mmbiz_png/J9oTXkdKsW80C2VQs0icYG0rK9nd8kqslA3mZIvystzqcic8L4LRtyb4bkNJLWFZnqaR4VlGibjHFyR38k9nmjqaA/0?wx_fmt=png',
+							medicine_name: '',
+							medicine_msg: '降压药'
+						},{
+							medicine_url: 'https://mmbiz.qpic.cn/mmbiz_png/J9oTXkdKsW80C2VQs0icYG0rK9nd8kqslA3mZIvystzqcic8L4LRtyb4bkNJLWFZnqaR4VlGibjHFyR38k9nmjqaA/0?wx_fmt=png',
+							medicine_name: '',
+							medicine_msg: '降压药'
+						},{
+							medicine_url: 'https://mmbiz.qpic.cn/mmbiz_png/J9oTXkdKsW80C2VQs0icYG0rK9nd8kqslA3mZIvystzqcic8L4LRtyb4bkNJLWFZnqaR4VlGibjHFyR38k9nmjqaA/0?wx_fmt=png',
+							medicine_name: '',
+							medicine_msg: '降压药'
+						},{
+							medicine_url: 'https://mmbiz.qpic.cn/mmbiz_png/J9oTXkdKsW80C2VQs0icYG0rK9nd8kqslA3mZIvystzqcic8L4LRtyb4bkNJLWFZnqaR4VlGibjHFyR38k9nmjqaA/0?wx_fmt=png',
+							medicine_name: '',
+							medicine_msg: '降压药'
+						},{
+							medicine_url: 'https://mmbiz.qpic.cn/mmbiz_png/J9oTXkdKsW80C2VQs0icYG0rK9nd8kqslA3mZIvystzqcic8L4LRtyb4bkNJLWFZnqaR4VlGibjHFyR38k9nmjqaA/0?wx_fmt=png',
+							medicine_name: '',
+							medicine_msg: '降压药'
+						},{
+							medicine_url: 'https://mmbiz.qpic.cn/mmbiz_png/J9oTXkdKsW80C2VQs0icYG0rK9nd8kqslA3mZIvystzqcic8L4LRtyb4bkNJLWFZnqaR4VlGibjHFyR38k9nmjqaA/0?wx_fmt=png',
+							medicine_name: '',
+							medicine_msg: '降压药'
+						},{
+							medicine_url: 'https://mmbiz.qpic.cn/mmbiz_png/J9oTXkdKsW80C2VQs0icYG0rK9nd8kqslA3mZIvystzqcic8L4LRtyb4bkNJLWFZnqaR4VlGibjHFyR38k9nmjqaA/0?wx_fmt=png',
+							medicine_name: '',
+							medicine_msg: '降压药'
+						},{
+							medicine_url: 'https://mmbiz.qpic.cn/mmbiz_png/J9oTXkdKsW80C2VQs0icYG0rK9nd8kqslA3mZIvystzqcic8L4LRtyb4bkNJLWFZnqaR4VlGibjHFyR38k9nmjqaA/0?wx_fmt=png',
+							medicine_name: '',
+							medicine_msg: '降压药'
+						},{
+							medicine_url: 'https://mmbiz.qpic.cn/mmbiz_png/J9oTXkdKsW80C2VQs0icYG0rK9nd8kqslA3mZIvystzqcic8L4LRtyb4bkNJLWFZnqaR4VlGibjHFyR38k9nmjqaA/0?wx_fmt=png',
 							medicine_name: '',
 							medicine_msg: '降压药'
 						}]
@@ -55,15 +103,15 @@
 					{
 						cate: '降糖药',
 						List: [{
-							medcine_url: '../static/yaopian.png',
+							medicine_url: 'https://mmbiz.qpic.cn/mmbiz_png/J9oTXkdKsW80C2VQs0icYG0rK9nd8kqslA3mZIvystzqcic8L4LRtyb4bkNJLWFZnqaR4VlGibjHFyR38k9nmjqaA/0?wx_fmt=png',
 							medicine_name: '',
 							medicine_msg: '降糖药'
 						}, {
-							medcine_url: '../static/yaopian.png',
+							medicine_url: 'https://mmbiz.qpic.cn/mmbiz_png/J9oTXkdKsW80C2VQs0icYG0rK9nd8kqslA3mZIvystzqcic8L4LRtyb4bkNJLWFZnqaR4VlGibjHFyR38k9nmjqaA/0?wx_fmt=png',
 							medicine_name: '',
 							medicine_msg: '降糖药'
 						}, {
-							medcine_url: '../static/yaopian.png',
+							medicine_url: 'https://mmbiz.qpic.cn/mmbiz_png/J9oTXkdKsW80C2VQs0icYG0rK9nd8kqslA3mZIvystzqcic8L4LRtyb4bkNJLWFZnqaR4VlGibjHFyR38k9nmjqaA/0?wx_fmt=png',
 							medicine_name: '',
 							medicine_msg: '降糖药'
 						}]
@@ -71,7 +119,7 @@
 					{
 						cate: '降脂药',
 						List: [{
-							medcine_url: '../static/yaopian.png',
+							medicine_url: 'https://mmbiz.qpic.cn/mmbiz_png/J9oTXkdKsW80C2VQs0icYG0rK9nd8kqslA3mZIvystzqcic8L4LRtyb4bkNJLWFZnqaR4VlGibjHFyR38k9nmjqaA/0?wx_fmt=png',
 							medicine_name: '',
 							medicine_msg: '降脂药'
 						}]
@@ -95,7 +143,6 @@
 						cate: '其他药物'
 					},
 				],
-				// medicineList: [],
 				active: 0
 			};
 		},
@@ -109,6 +156,12 @@
 				this.active = i
 				//切换分类
 				this.medicineList = this.cateList[i].List
+			},
+				
+			gotoDetail(item) {
+				uni.navigateTo({
+					url:'/subpkg/detail/detail?item=' +item
+				})
 			}
 		}
 	}
@@ -147,13 +200,41 @@
 		}
 	}
 
-	.medicine_msg {
+	.medicine-msg {
 		display: flex;
-		height: 50rpx;
+		padding: 10px 5px;
+		border-bottom: 1px solid #f0f0f0;
 		
-		.medicine_img {
-			width: 25rpx;
-			height: 80%;
+		.medicine-msg-left {
+			margin-right: 5px;
+			.medicine-pic {
+				width: 200rpx;
+				height: 200rpx;
+				display: block;
+			}
 		}
+		.medicine-msg-right {
+			display: flex;
+			flex-direction: column;
+			justify-content: space-between;
+			
+			.medicine-name {
+				font-size: 16px;
+				line-height: 50%;
+			}
+			
+			.medicine-info-box {
+				.medicine-info {
+					font-size: 13px;
+				}
+			}
+		}
+		// display: flex;
+		// height: 50rpx;
+		
+		// .medicine_img {
+		// 	width: 25rpx;
+		// 	height: 80%;
+		// }
 	}
 </style>
